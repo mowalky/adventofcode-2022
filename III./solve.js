7d5a33d7bbf5875f7180ea8d794a1b03d6afcd66
mowalky <mowalky@outlook.com>
2022-12-07 23:06:37 -0600

    updated

--- III./solve.js
+++ III./solve.js
@@ -1,13 +1,13 @@
 /**
  *
- * Q. Find the item type that appears in both compartments of
- *    each rucksack. What is the sum of the priorities of those
- *    item types?
+ *  Q. Find the item type that appears in both compartments of
+ *     each rucksack. What is the sum of the priorities of those
+ *     item types?
  *
- * 1. separate into 2 compartments (divide in half)
- * 2. find common item type that appears in BOTH comparments
- * 3. find the priority of common item type
- * 4. sum the priorities of the item types
+ *   1. separate into 2 compartments (divide in half)
+ *   2. find common item type that appears in BOTH comparments
+ *   3. find the priority of common item type
+ *   4. sum the priorities of the item types
  *
  */
 
