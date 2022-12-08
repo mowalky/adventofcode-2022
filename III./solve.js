2c6149a5f1b31e6dd36525b425b7570da6493b79
mowalky <mowalky@outlook.com>
2022-12-07 22:13:17 -0600

    setup of day 3

---
+++ III./solve.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * Q. Find the item type that appears in both compartments of
+ *    each rucksack. What is the sum of the priorities of those
+ *    item types?
+ *
+ * 1. separate into 2 comparments (divide in half)
+ * 2. find common item type that appears in BOTH comparments
+ * 3. find the priority of common item type
+ * 4. sum the priorities of the item types
+ *
+ */
