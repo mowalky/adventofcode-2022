12ecdad555c81f7b060df8b5e0ae139622918f74
mowalky <mowalky@outlook.com>
2022-12-08 22:01:34 -0600

    updated

--- III./solve.js
+++ III./solve.js
@@ -14,11 +14,34 @@
 var fs = require("fs");
 const rucksacks = fs.readFileSync("test.txt", "utf8").split(/\r?\n/);
 
+function itemPriority(item) {
+  let priority;
+  if (item == item.toLowerCase()) {
+    priority = item.charCodeAt(0) - 97 + 1;
+  } else {
+    priority = item.toLowerCase().charCodeAt(0) - 97 + 27;
+  }
+
+  console.log(priority);
+}
+
+function commonItems(comp1, comp2) {
+  let commonItem;
+  let spreadOutComp1 = comp1.split("");
+  let spreadOutComp2 = comp2.split("");
+  spreadOutComp2.forEach((c) => {
+    if (spreadOutComp1.includes(c)) commonItem = c;
+  });
+  console.log("common item: ", commonItem);
+  itemPriority(commonItem);
+}
+
 function compartments(rucksack) {
   const compartmentOne = rucksack.slice(0, rucksack.length / 2);
   const compartmentTwo = rucksack.slice(rucksack.length / 2, rucksack.length);
   console.log("Compartment #1 ", compartmentOne);
   console.log("Compartment #2 ", compartmentTwo);
+  commonItems(compartmentOne, compartmentTwo);
   console.log("====\n");
 }
 
