f37e8103ff8c3b012af32d76e2eeb7c61c00397c
mowalky <mowalky@outlook.com>
2022-12-10 12:23:26 -0600

    updated

--- IV./solve.js
+++ IV./solve.js
@@ -3,5 +3,13 @@ const assignments = fs.readFileSync("test.txt", "utf8").split(/\r?\n/);
 
 assignments.forEach((assignment) => {
   const [pairOne, pairTwo] = assignment.split(",");
-  console.log(pairOne);
+
+  const [pairOneStart, pairOneEnd] = pairOne.split("-");
+  const [pairTwoStart, pairTwoEnd] = pairTwo.split("-");
+
+  if (pairTwoStart <= pairOneStart && pairTwoStart <= pairOneEnd) {
+    console.log("contained!");
+  }
+
+  console.log(pairOneStart);
 });
