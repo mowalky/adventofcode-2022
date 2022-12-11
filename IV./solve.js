d88e579397e37f1e3fe3bb7404530aa233edf253
mowalky <mowalky@outlook.com>
2022-12-10 21:58:25 -0600

    solved part 1

--- IV./solve.js
+++ IV./solve.js
@@ -1,15 +1,24 @@
 var fs = require("fs");
-const assignments = fs.readFileSync("test.txt", "utf8").split(/\r?\n/);
+const assignments = fs.readFileSync("input.txt", "utf8").split(/\r?\n/);
+
+let total = 0;
 
 assignments.forEach((assignment) => {
   const [pairOne, pairTwo] = assignment.split(",");
 
-  const [pairOneStart, pairOneEnd] = pairOne.split("-");
-  const [pairTwoStart, pairTwoEnd] = pairTwo.split("-");
+  const [pairOneStart, pairOneEnd] = pairOne.split("-").map(Number);
+  const [pairTwoStart, pairTwoEnd] = pairTwo.split("-").map(Number);
 
-  if (pairTwoStart <= pairOneStart && pairTwoStart <= pairOneEnd) {
+  console.log(`1: ${pairOne} 2: ${pairTwo}`);
+  console.log(`start: ${pairOneStart} = ${pairTwoStart}`);
+  console.log(`end: ${pairOneEnd} = ${pairTwoEnd}`);
+  if (
+    (pairTwoStart >= pairOneStart && pairTwoEnd <= pairOneEnd) ||
+    (pairOneStart >= pairTwoStart && pairOneEnd <= pairTwoEnd)
+  ) {
     console.log("contained!");
+    total++;
   }
-
-  console.log(pairOneStart);
+  console.log(`===`);
 });
+console.log(total);
