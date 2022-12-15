533347b110cf2bb567f1339d1bb373a4ed2abd36
mowalky <mowalky@outlook.com>
2022-12-14 23:06:31 -0600

    solved part 1

--- V./solve.js
+++ V./solve.js
@@ -12,10 +12,20 @@
 
  */
 
-let stacks = [["N", "Z"], ["D", "C", "M"], ["P"]];
+let stacks = [
+  ["R", "H", "M", "P", "Z"],
+  ["B", "J", "C", "P"],
+  ["D", "C", "L", "G", "H", "N", "S"],
+  ["L", "R", "S", "Q", "D", "M", "T", "F"],
+  ["M", "Z", "T", "B", "Q", "P", "S", "F"],
+  ["G", "B", "Z", "S", "F", "T"],
+  ["V", "R", "N"],
+  ["M", "C", "V", "D", "T", "L", "G", "P"],
+  ["L", "M", "F", "J", "N", "Q", "W"],
+];
 
 var fs = require("fs");
-const moves = fs.readFileSync("test.txt", "utf8").split(/\r?\n/);
+const moves = fs.readFileSync("input.txt", "utf8").split(/\r?\n/);
 
 function moveCrate(from, to) {
   let crate = stacks[from - 1][0]; // get top crate
